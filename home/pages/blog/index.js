e143911c444a02e880d9adad60353942baf5233d
thuynt <thuynt@bit-vietnam.com>
2020-11-12 13:31:37 +0700

    Add blog to home app

---
+++ home/pages/blog/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <p>This is our blog hom page</p>
+      <div>
+        <a href="/blog">Blog list</a>
+      </div>
+      <img width={200} src="/static/nextjs2.png" />
+    </div>
+  )
+}
